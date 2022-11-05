078547c2fae3e648472d57afc3eeb78c5432b000
Дмитрий Дмитриевич Берестнев <d.berestnev@malltech.ru>
2022-11-05 17:57:15 +0600

    fix autoload :hammer:

--- App/Components/Autoload.php
+++ App/Components/Autoload.php
@@ -2,16 +2,9 @@
 
 function __autoload($class_name)
 {
-    $array_paths = array(
-        'Store/Services',
-    );
-
-    foreach($array_paths as $path){
-       
-        $path = str_replace('\\', DIRECTORY_SEPARATOR,ROOT .'/'.$class_name.'.php');
+    $path = str_replace('\\', DIRECTORY_SEPARATOR,ROOT .'/'.$class_name.'.php');
         
-        if(is_file($path)){
-            include_once($path);
-        }
+    if(is_file($path)){
+      include_once($path);
     }
 }
